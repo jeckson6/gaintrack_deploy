fd45432fcf44404da571f06b2e73c5b4281f50a1
jeckson6 <tp064611@mail.apu.edu.my>
2026-01-06 14:40:37 +0800

    app

--- backend/app.js
+++ backend/app.js
@@ -1,49 +1,60 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const express = require("express");
-const cors = require("cors");
-const userRoutes = require("./routes/userRoutes");
-const healthRoutes = require("./routes/healthRecordRoutes");
-const aiRoutes = require("./routes/aiRoutes");
-const imageRoutes = require("./routes/imageRoutes"); 
-const foodPlanRoutes = require("./routes/foodPlanRoutes");
-const trainingPlanRoutes = require("./routes/trainingPlanRoutes");
-const adminRoutes = require("./routes/adminRoutes");
-const systemconfig =require("./routes/systemConfigRoutes")
+// const express = require("express");
+// const cors = require("cors");
+// const userRoutes = require("./routes/userRoutes");
+// const healthRoutes = require("./routes/healthRecordRoutes");
+// const aiRoutes = require("./routes/aiRoutes");
+// const imageRoutes = require("./routes/imageRoutes"); 
+// const foodPlanRoutes = require("./routes/foodPlanRoutes");
+// const trainingPlanRoutes = require("./routes/trainingPlanRoutes");
+// const adminRoutes = require("./routes/adminRoutes");
+// const systemconfig =require("./routes/systemConfigRoutes")
 
 
+
+// const app = express();
+
+// console.log("DB_USER:", process.env.DB_USER);
+// app.get("/api/health-check", (req, res) => {
+//   res.status(200).json({
+//     status: "ok",
+//     message: "GainTrack backend is running"
+//   });
+// });
+
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     "https://gaintrack-deploy.vercel.app"
+//   ]
+// }));
+// app.use(express.json());
+
+// //user routes
+// app.use("/api/users", userRoutes);
+// app.use("/api/health", healthRoutes);
+// app.use("/api/ai", aiRoutes);
+// app.use("/api/images", imageRoutes); 
+// app.use("/api/food-plan", foodPlanRoutes);
+// app.use("/api/training-plan", trainingPlanRoutes);
+// app.use("/api/system-config", systemconfig);
+
+// //admin routes
+// app.use("/api/admins", adminRoutes);
+
+// // app.listen(5000, () => {
+// //   console.log("Server running on port 5000");
+// // });
+// module.exports = app;const express = require("express");
 
 const app = express();
 
-console.log("DB_USER:", process.env.DB_USER);
 app.get("/api/health-check", (req, res) => {
   res.status(200).json({
     status: "ok",
-    message: "GainTrack backend is running"
+    message: "Minimal backend works on Vercel"
   });
 });
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://gaintrack-deploy.vercel.app"
-  ]
-}));
-app.use(express.json());
-
-//user routes
-app.use("/api/users", userRoutes);
-app.use("/api/health", healthRoutes);
-app.use("/api/ai", aiRoutes);
-app.use("/api/images", imageRoutes); 
-app.use("/api/food-plan", foodPlanRoutes);
-app.use("/api/training-plan", trainingPlanRoutes);
-app.use("/api/system-config", systemconfig);
-
-//admin routes
-app.use("/api/admins", adminRoutes);
-
-// app.listen(5000, () => {
-//   console.log("Server running on port 5000");
-// });
 module.exports = app;
